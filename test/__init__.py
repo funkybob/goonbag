bf00f72e22680efa6e2dabd562142788e48bc15b
Curtis Maloney <curtis@tinbrain.net>
2017-08-27 13:38:16 +1000

    Add test app

---
+++ test/__init__.py
@@ -0,0 +1,17 @@
+from goonbag.wsgi import WsgiApplication
+from goonbag import Routes, Handler
+
+api = Routes()
+
+
+@api.route('/')
+def index(request):
+    return 'Welcome!'
+
+@api.route('/greet')
+class Greet(Handler):
+    def get(self, request):
+        return 'Hello, {}'.format(request.query.get('name', 'friend'))
+
+
+application = WsgiApplication(api)
