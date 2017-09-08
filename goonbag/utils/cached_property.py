2e48014b0415fb4ce4ce3a765d743adddc670112
Curtis Maloney <curtis@tinbrain.net>
2017-09-08 22:02:33 +1000

    Allow overriding name on cached_property

--- goonbag/utils/cached_property.py
+++ goonbag/utils/cached_property.py
@@ -1,11 +1,12 @@
 
 
 class cached_property:
-    def __init__(self, func):
+    def __init__(self, func, name=None):
         self.func = func
+        self.name = name or func.__name__
 
     def __get__(self, instance, cls=None):
         if instance is None:
             return self
-        value = instance.__dict__[self.func.__name__] = self.func(instance)
+        value = instance.__dict__[self.name] = self.func(instance)
         return value
